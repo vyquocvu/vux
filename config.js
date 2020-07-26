87f39d64b018eadcf55b8b3f8f92c9e3c9ed0e2a
vuvyquoc <vyquocvu@gmail.com>
2020-07-26 16:31:32 +0700

    Fix deploy

---
+++ config.js
@@ -0,0 +1,17 @@
+export default {
+  "name": "Duostack Developer",
+  "title": "Blog của Vũ",
+  "author": "Vu Vy",
+  "content": "Tôi là Vũ, code dạo cũng được vài năm, không dám nhận mình là fullstack nhưng mà cần làm cái gì thì sẽ làm cái đó.",
+  "avatar": "https://github.com/vyquocvu.png",
+  "pages": [
+    { "path": "/", "label": "Home" },
+    { "path": "/about", "label": "About" },
+    { "path": "/contact", "label": "Contact" }
+  ],
+  "socials": [
+    { "url": "https://fb.com/vyquocvu", "label": "Facebook", "icon": "/icons/facebook.svg" },
+    { "url": "https://linkedin.com/in/vyquocvu/", "label": "Linkedin", "icon": "/icons/linkedin.svg" },
+    { "url": "https://github.com/vyquocvu", "label": "Github", "icon": "/icons/github.svg" }
+  ]
+}
