e2555b0e56739ad03ef78e07bd39cf2420da7d30
vuvyquoc <vyquocvu@gmail.com>
2021-03-31 17:35:19 +0700

    yarn.lock update

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
-
 import config from 'config';
 
 const SideBar = () => (
