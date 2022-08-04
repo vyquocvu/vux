7d730067c6563fbed84d69975ec0f74304e56a5f
vyquocvu <vyquocvu@gmail.com>
2022-08-05 00:10:08 +0700

    update gtag id

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,17 +25,25 @@ class CustomDocument extends Document<Props> {
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
-          <Script
-            src="https://www.googletagmanager.com/gtag/js?id=GTM-THCFN5G"
-            strategy="afterInteractive"
-          />
+
+          <Script id="gtag" strategy="beforeInteractive">
+            {
+              `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-THCFN5G');`
+            }
+          </Script>
+          {/* TAg */}'}
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-9MDL41Z920" />
           <Script id="google-analytics" strategy="afterInteractive">
             {`
               window.dataLayer = window.dataLayer || [];
-              function gtag(){window.dataLayer.push(arguments);}
+              function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-
-              gtag('config', 'GTM-THCFN5G');
+              gtag('config', 'G-9MDL41Z920');
             `}
           </Script>
         </Head>
